e161e02b40b01c253a13417a95a16cb4a353d2de
luoway <luoway@outlook.com>
2023-08-17 15:52:30 +0800

    test offline

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,7 @@
 importScripts('https://cdn.bootcdn.net/ajax/libs/workbox-sw/6.5.4/workbox-sw.min.js')
 
+workbox.routing.setDefaultHandler(new workbox.strategies.StaleWhileRevalidate())
 workbox.recipes.pageCache()
 workbox.recipes.staticResourceCache()
 workbox.recipes.imageCache()
+workbox.recipes.offlineFallback()
